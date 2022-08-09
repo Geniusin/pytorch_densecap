989703cb4e61e4fe60147a157acbc538c8406e3e
Geniusin <genius0re@naver,com>
2022-08-09 21:33:27 +0900

    f

--- demo.py
+++ demo.py
@@ -1,1 +1,17 @@
-print('hi')
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+
+image = torch.zeros((1, 3, 600, 800)).float()
+image_size = (600, 800)
+labels = torch.LongTensor([6, 8])
+
+
+backbone = torchvision.models.vgg16(pretrained=True)
+feat = list(backbone.features)
+req_features = feat[:30]
+image_feature = nn.Sequential(*req_features)
+
+backbone_out =
